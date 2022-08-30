d441c345d22f73816b7cbe70149c1369040c2801
jmmirza <janmuhammadmirza@gmail.com>
2022-08-30 19:01:51 +0500

    changes

--- src/app/modals/create-wagers/create-wagers.module.ts
+++ src/app/modals/create-wagers/create-wagers.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CreateWagersPageRoutingModule } from './create-wagers-routing.module';
 import { CreateWagersPage } from './create-wagers.page';
@@ -8,9 +8,10 @@ import { CreateWagersPage } from './create-wagers.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    CreateWagersPageRoutingModule
+    CreateWagersPageRoutingModule,
   ],
-  declarations: [CreateWagersPage]
+  declarations: [CreateWagersPage],
 })
 export class CreateWagersPageModule {}
