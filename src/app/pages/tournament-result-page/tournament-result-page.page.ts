73af288da4cff7b7e311cef56f82dca043c0b8e7
jmmirza <janmuhammadmirza@gmail.com>
2022-10-18 17:03:49 +0500

    changes

--- src/app/pages/tournament-result-page/tournament-result-page.page.ts
+++ src/app/pages/tournament-result-page/tournament-result-page.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll, LoadingController } from '@ionic/angular';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-tournament-result-page',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournament-result-page.page.scss'],
 })
 export class TournamentResultPagePage implements OnInit {
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+  pageNo = 1;
+  response: any;
+  platforms: any;
+  wagerPostRequestList: any;
+  segment = 'wager_matches';
 
-  constructor() { }
+  filters = {
+    matchStatus: '',
+    type: 'requests',
+  };
 
-  ngOnInit() {
+  constructor(
+    private loadingCtrl: LoadingController,
+    private toastService: ToastService
+  ) {}
+
+  ngOnInit() {}
+
+  async loadMore(event) {
+    this.pageNo++;
+    // await this.wagerService.listWagers({ page_no: this.pageNo }).subscribe(
+    //   (data: any) => {
+    //     console.log(data);
+
+    //     for (var item of data.wagers) {
+    //       this.response.push(item);
+    //     }
+
+    //     event.target.complete();
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //     event.target.complete();
+    //   }
+    // );
   }
 
+  async segmentChanged(ev) {
+    console.log(ev.detail.value);
+
+    this.filters.matchStatus = ev.detail.value;
+    this.segment = ev.detail.value;
+    // await this.listWagers(this.filters);
+  }
+
+  async listMatches(matchStatus) {
+    console.log(matchStatus);
+    this.filters.matchStatus = matchStatus;
+    // await this.listWagers(this.filters);
+  }
 }
